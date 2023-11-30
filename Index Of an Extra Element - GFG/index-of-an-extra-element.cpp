abb56dbb10faf66d46f02cb66ad0400f0fc3a094
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2023-12-01 00:29:51 +0530

    Added solution - LeetHub

---
+++ Index Of an Extra Element - GFG/index-of-an-extra-element.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int findExtra(int a[], int b[], int n) {
+        // add code here.
+        int s=0,e=n-1,mid;
+        int ans=n-1;
+        while(s<=e)
+        {
+             mid=s+(e-s)/2;
+            
+            if(a[mid]==b[mid])
+            s=mid+1;
+            else
+            {
+                ans=mid;
+                e=mid-1;
+            }
+            
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], b[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> b[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(a, b, n) << endl;
+    }
+}
+// } Driver Code Ends
