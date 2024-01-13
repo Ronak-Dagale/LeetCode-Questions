58f9c9d3436e1eede3c75f8d5e6b0f828d3aff9c
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-01-13 12:38:03 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (56.93%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        int n=s.size()-1;
+        
+        while(n>=0)
+        {
+            while(s[n--]==' ');
+            n++;
+            
+            while(n>=0 && s[n--]!=' ')
+            {
+                ans++;
+            }
+                break;
+        }
+        return ans;
+    }
+};
