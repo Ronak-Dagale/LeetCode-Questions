5c5c5428454f407e4476fe78f1a03f78d99064ed
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-01-14 20:06:14 +0530

    Time: 10 ms (60.69%), Space: 8.7 MB (24.88%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        int ans=0;
-        unordered_map<string,int>mp;
-        mp["IV"]=4;
-        mp["IX"]=9;
-        mp["XL"]=40;
-        mp["XC"]=90;
-        mp["CD"]=400;
-        mp["CM"]=900;
-        
-        mp["I"]=1;
-        mp["V"]=5;
-          mp["X"]=10;
-          mp["L"]=50;
-          mp["C"]=100;
-         mp["D"]=500;
-        mp["M"]=1000;
-        for(int i=0;i<s.size();i++)
-        {
-            if(i+1<s.size())
-            {
-                string temp="";
-                temp.push_back(s[i]);
-                temp.push_back(s[i+1]);
-                if(mp.find(temp)!=mp.end())
-               {
-                    ans=ans+mp[temp];
-                    i++;
-                    continue;
-               }
-                
-            }
-            
-            
-                string temp="";
-                temp.push_back(s[i]);
-                ans=ans+mp[temp];
-               
-            
-        }
-        return ans;
+    int romanToInt(string str) {
+         map<char,int>mp;
+        mp['I']=1;
+         mp['V']=5;
+          mp['X']=10;
+           mp['L']=50;
+            mp['C']=100;
+             mp['D']=500;
+             mp['M']=1000;
+             
+             int i=str.size()-1;
+             int ans=0;
+             
+             ans=mp[str[i]];
+             i--;
+             while(i>=0)
+             {
+                 int prev=mp[str[i+1]];
+                 int ne=mp[str[i]];
+                 
+                 if(prev>ne)
+                 ans=ans-ne;
+                 else
+                 ans=ans+ne;
+                 
+                 i--;
+             }
+             return ans;
     }
 };
