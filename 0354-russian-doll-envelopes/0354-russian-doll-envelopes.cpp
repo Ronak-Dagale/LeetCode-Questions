d8c15e1d01d5c69819b1fcebbf0c11b541b0b391
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-01-27 00:33:48 +0530

    Time: 185 ms (98.96%), Space: 81.1 MB (26.38%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-   static bool cmp(vector<int>a,vector<int>b)
-    {
-        if(a[0]!=b[0])
-            return a[0]<b[0];
-        else
-            return  a[1]>b[1];
+      static bool compare(vector<int>& a, vector<int>& b){
+        if(a[0]==b[0])
+            return a[1]>b[1];
+        return a[0]<b[0];
     }
-    int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(envelopes.begin(),envelopes.end(),cmp);
-        
-       int n=envelopes.size();
-        vector<int>ans;
+    int mostOptimal(vector<vector<int>>& envelopes){
+       
+        vector<int> ans;
         ans.push_back(envelopes[0][1]);
-        for(int i=1;i<n;i++)
-        {
+        for(int i=1;i<envelopes.size();i++){
             if(envelopes[i][1]>ans.back())
                 ans.push_back(envelopes[i][1]);
-            else
-            {
-              int id=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
-                ans[id]=envelopes[i][1];
+            else{
+                int index=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
+                ans[index]=envelopes[i][1];
             }
         }
         return ans.size();
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        
+        sort(envelopes.begin(),envelopes.end(),compare);
+        int prev=-1;
+        int curr=0;
+        return mostOptimal(envelopes);
+
+         
     }
 };
