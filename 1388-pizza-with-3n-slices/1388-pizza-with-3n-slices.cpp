1dd3ab75bf6d150305ffb73e27c58879b69bd53e
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-01-30 19:47:49 +0530

    Time: 7 ms (97.67%), Space: 25 MB (7.31%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    int solve(vector<int>& slices,int id,int endid,int n,vector<vector<int>>&dp)
-    {
-       if(n==0 || id>endid) return 0;
-        
-        if(dp[id][n]!=-1) return dp[id][n];
-        int take=slices[id]+solve(slices,id+2,endid,n-1,dp);
-        int nottake=solve(slices,id+1,endid,n,dp);
-        
-        return dp[id][n]=max(take,nottake);
-    }
+  
     int maxSizeSlices(vector<int>& slices) {
         int k=slices.size();
         
-        vector<vector<int>>dp1(k,vector<int>(k,-1));
-        vector<vector<int>>dp2(k,vector<int>(k,-1));
-        int case1=solve(slices,0,k-2,k/3,dp1);
-        int case2=solve(slices,1,k-1,k/3,dp2);
+        vector<vector<int>>dp1(k+2,vector<int>(k,0));
+        vector<vector<int>>dp2(k+2,vector<int>(k,0));
+        
+        for(int id=k-2;id>=0;id--)
+        {
+            for(int n=1;n<=k/3;n++)
+            {
+              int take=slices[id]+dp1[id+2][n-1];
+        int nottake=dp1[id+1][n];
+        
+       dp1[id][n]=max(take,nottake);
+            }
+        }
+        
+        int case1=dp1[0][k/3];
+        
+         for(int id=k-1;id>=1;id--)
+        {
+            for(int n=1;n<=k/3;n++)
+            {
+              int take=slices[id]+dp2[id+2][n-1];
+              int nottake=dp2[id+1][n];
+        
+       dp2[id][n]=max(take,nottake);
+            }
+        }
+        
+        int case2=dp2[1][k/3];
         
         return max(case1,case2);
     }
