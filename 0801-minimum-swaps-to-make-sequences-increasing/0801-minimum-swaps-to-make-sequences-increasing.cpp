ba6102a6001b76c9a6b2fff803835b34888b1456
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-01-31 23:53:27 +0530

    Time: 178 ms (67.86%), Space: 122.9 MB (46.28%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -30,7 +30,33 @@ public:
         nums2.insert(nums2.begin(),-1);
         
         int n=nums1.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return solve(nums1,nums2,1,0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        
+        for(int id=n-1;id>=1;id--)
+        {
+            for(int isswap=0;isswap<=1;isswap++)
+            {
+                
+        int ans=INT_MAX;
+        
+        int prev1=nums1[id-1];
+        int prev2=nums2[id-1];
+        
+        if(isswap)
+            swap(prev1,prev2);
+        
+        // noswap cond
+        if(prev1<nums1[id] && prev2<nums2[id])
+           ans=dp[id+1][0];
+        
+        // swap when
+        if(prev2<nums1[id] && prev1<nums2[id])
+            ans=min(ans,1+dp[id+1][1]);
+        
+         dp[id][isswap]=ans;
+            }
+        }
+        return (dp[1][0]);
+        
     }
 };
