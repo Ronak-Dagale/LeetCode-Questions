9775e15a5727c9e186cbc8945e0ac1c84ef9ba2a
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-02-05 15:14:36 +0530

    Time: 23 ms (80.90%), Space: 12.1 MB (23.37%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<vector<int>>mp(26,vector<int>(2,0));
+        
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]-'a'][0]++;
+            mp[s[i]-'a'][1]=i;
+        }
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(mp[s[i]-'a'][0]==1) return mp[s[i]-'a'][1];
+        }
+        return -1;
+    }
+};
