58c7ec5d60fd6f49f493d1607c894b684659a308
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-03-31 13:19:05 +0530

    Time: 197 ms (13.06%), Space: 105.9 MB (7.96%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -3,17 +3,14 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
         int ans=0;
-        vector<int>oddcnt(n,0);
         unordered_map<int,int>mp;
         mp[0]=1;
         int cnt=0;
         for(int i=0;i<n;i++)
         {
             if(nums[i]&1)cnt++;
-            
-            int ele=cnt-k;
-            ans+=mp[ele];
-            
+            //  int ele=cnt-k;
+            ans+=mp[cnt-k]; 
             mp[cnt]++;
         }
         return ans;
