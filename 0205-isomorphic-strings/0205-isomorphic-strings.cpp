bff76c951601b211b31b1aecae9ad836b884c709
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-04-02 18:14:45 +0530

    Time: 6 ms (65.28%), Space: 8.2 MB (77.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mp1,mp2;
+        // instead of string each charachter 
+        // store the index of previous occurance
         
-        for(int i=0;i<s.size();i++)
-        {mp1[s[i]]=t[i];
-          mp2[t[i]]=s[i];
-        }
+        vector<int>id1(200,-1);
+        vector<int>id2(200,-1);
         
         for(int i=0;i<s.size();i++)
         {
-            if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i])
+            if(id1[s[i]]!=id2[t[i]])
                 return 0;
+            
+            id1[s[i]]=i+1;
+            id2[t[i]]=i+1;
         }
         
         return 1;
