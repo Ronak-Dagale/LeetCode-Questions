3459c4f19b11e9f4ad47bc0d08b254cbe756f2e4
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-04-04 15:41:53 +0530

    Time: 3 ms (40.00%), Space: 7.5 MB (22.85%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans=0,cnt=0;
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){cnt++;
+                ans=max(ans,cnt); }
+            else if(s[i]==')') cnt--;
+        }
+        return ans;
+    }
+};
