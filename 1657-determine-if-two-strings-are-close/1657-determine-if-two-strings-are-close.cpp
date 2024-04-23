aff3643288c29ba90cdc938e3ca4da54c4e1ddb8
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-04-23 19:55:24 +0530

    Time: 64 ms (78.54%), Space: 21.8 MB (95.27%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -5,30 +5,24 @@ public:
         vector<int>f2(26,0);
         
         for(int i=0;i<word1.size();i++)
-        {
-            char ch=word1[i];
-            f1[ch-'a']++;
-        }
-          
+         f1[word1[i]-'a']++;
         
+         
          for(int i=0;i<word2.size();i++)
-        {
-            char ch=word2[i];
-            f2[ch-'a']++;
-        }
+            f2[word2[i]-'a']++;
         
         for(int i=0;i<26;i++)
-        {
-          if((f1[i]==0 && f2[i]!=0) || (f1[i]!=0 && f2[i]==0)) return 0;
-        }
+        if((f1[i]==0 && f2[i]!=0) || (f1[i]!=0 && f2[i]==0)) 
+        return 0;
+        
         
         sort(f1.begin(),f1.end());
         sort(f2.begin(),f2.end());
         
         for(int i=0;i<26;i++)
-        {
-            if(f1[i]!=f2[i]) return 0;
-        }
+        if(f1[i]!=f2[i]) 
+            return 0;
+        
         return 1;
         
     }
