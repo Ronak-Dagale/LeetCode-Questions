61ae5ef68d87d932399b8ea64e2ce56b9cc79bee
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-05-09 20:22:45 +0530

    Time: 160 ms (61.40%), Space: 107.2 MB (60.15%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        long long ans=0;
+        
+         sort(happiness.begin(),happiness.end(),greater<int>());
+        for(int i=0;i<k;i++)
+        {
+            if(happiness[i]-i>=0)
+                ans=ans+happiness[i]-i;
+            
+//           cout<<happiness[i]<<" "<<i<<endl;
+        }
+        return ans;
+    }
+};
