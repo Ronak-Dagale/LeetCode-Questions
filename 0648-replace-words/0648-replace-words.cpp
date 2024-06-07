e33b3856a30222c8d8025ef23ef641a633a7e497
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-06-07 10:58:09 +0530

    Time: 225 ms (29.65%), Space: 46.4 MB (85.70%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string>s;
-        for(int i=0;i<dictionary.size();i++)
-            s.insert(dictionary[i]);
-        
+        unordered_set<string>s(dictionary.begin(),dictionary.end());
+ 
         int i=0;
         string ans="";
         while(i<sentence.size())
@@ -13,12 +11,12 @@ public:
             while(i<sentence.size() && sentence[i]!=' ')
             {
                 s1.push_back(sentence[i]);
+                ans.push_back(sentence[i]);
                 if(s.find(s1)!=s.end()){
                     break;
                 }
                 i++;
             }
-            ans+=s1;
             while(i<sentence.size() && sentence[i]!=' ')i++;
             if(i<sentence.size()) ans.push_back(' ');
             
