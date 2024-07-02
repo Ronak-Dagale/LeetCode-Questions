5a40e4a8bda430dee995df6119df68be7c90c6b8
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-07-02 18:51:31 +0530

    Time: 4 ms (59.42%), Space: 12.5 MB (75.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -3,17 +3,21 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
         
-        for(int i=0;i<nums1.size();i++)
+      sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size())
         {
-            for(int j=0;j<nums2.size();j++)
+            if(nums1[i]==nums2[j])
             {
-                if(nums1[i]==nums2[j])
-                {
-                    ans.push_back(nums1[i]);
-                    nums2[j]=-1;
-                    break;
-                }
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
             }
+            else if(nums1[i]>nums2[j]) j++;
+            else
+                i++;
         }
         return ans;
     }
