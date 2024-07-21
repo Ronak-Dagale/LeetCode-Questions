1b944e6ab60013313af50ba252ff84d70e53cec4
Ronak Dagale <114944774+Ronak-Dagale@users.noreply.github.com>
2024-07-21 17:59:10 +0530

    Time: 35 ms (100.00%), Space: 17.5 MB (20.00%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-                cnt++;
+                return 1;
         }
         if(cnt==0) return 0;
         return 1;
